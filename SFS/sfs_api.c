80b43c0825f405507bc4e29e1c3ad6cde0714709
EmolLi <duan.li@mail.mcgill.ca>
2016-11-27 15:59:13 -0500

    data structure and defined variable (not finished)

--- SFS/sfs_api.c
+++ SFS/sfs_api.c
@@ -1,6 +1,61 @@
 #include "sfs_api.h"
 #include "disk_emu.h"
 
+
+#define FILENAME "File System"
+#define BLOCK_SIZE 1024
+#define BLOCK_NUM 2000
+
+//all the lengths are #blk
+#define SUPER_BLOCK_LENGTH 1
+#define INODE_TABLE_LENGTH 200	//can hold 199 files, the first one is reserved for directory
+#define DATA_BLOCK_LENGTH 798
+#define FREE_BIT_MAP_LENGTH 1
+
+#define DIRECT_PTR_NUM 12
+#define OPEN_FILE_TABLE_SIZE 199
+#define MAX_FILE_BLK (DIRECT_PTR_NUM + BLOCK_SIZE/sizeof(int))
+
+#define SUPER_BLOCK_INDEX 0
+#define INODE_TABLE_INDEX SUPER_BLOCK_LENGTH
+#define DATA_BLOCK_INDEX (INODE_TABLE_INDEX + INODE_TABLE_LENGTH)
+#define FREE_BIT_MAP_INDEX (DATA_BLOCK_INDEX + DATA_BIT_MAP_LENGTH)
+
+//====================data structure=====================
+typedef struct super_block{
+	int magic;
+	int block_size;	//1024
+	int file_system_size;	//#blk
+	int inode_table_length;	//#blk
+} super_block;
+
+//64
+typedef struct inode{
+	int initialized;	//0 for uninitialized, 1 for initialized
+	int link_cnt;
+	int size;	//bytes
+	int direct_ptr[DIRECT_PTR_NUM];	//block index
+	int indirect_ptr;
+} inode;
+
+
+//open file table
+typedef	struct file{
+	//file descriptor = file ID = array index in open file table
+	int inode_index;
+	int readptr;
+	int writeptr;
+}file;
+
+typedef struct open_file_table{
+	file files[OPEN_FILE_TABLE_SIZE];
+} open_file_table;
+
+
+
+
+
+
 void mksfs(int fresh){
 
 }
@@ -31,3 +86,11 @@ int sfs_fread(int fileID, char *buf, int length){
 int sfs_remove(char *file){
   return 0;
 }
+
+
+
+
+
+
+
+
